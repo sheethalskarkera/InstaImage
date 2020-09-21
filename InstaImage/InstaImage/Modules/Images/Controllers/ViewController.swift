6c7a1d006822e5502546f43290d3b1b6dd858364
Sheethal Karkera <sheethalskarkera@gmail.com>
2020-09-21 22:04:46 +1000

    Initial commit.

---
+++ InstaImage/InstaImage/Modules/Images/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaImage
+//
+//  Created by Sheethal on 21/9/20.
+//  Copyright © 2020 Sheethal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
